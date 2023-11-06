3bff7ad1dda3bcb4ef71755c3dbf3cd20f3d9fdb
Ducaxias <eduardo.caxias90@gmail.com>
2023-11-06 20:15:37 -0300

    feat(Produtos): Mostra produtos.

--- Assets/js/produtos.js
+++ Assets/js/produtos.js
@@ -1,18 +1,21 @@
 const Produtos = [
     {
-      nome: "Corto Maltese: balada do mar salgado",
+      nome: "Corto Maltese <br> Balada do mar salgado",
       autor: "Hugo Pratt",
       img: 'Assets/Images/Corto.jpeg',
+      preço: 25
     },
     {
       nome: "Paraíso Perdido",
       autor: "Jhon Milton",
       img: 'Assets/Images/Paraiso.jpeg',
+      preço: 32
     },
     {
       nome: "Quincas Borba",
       autor: "Machado de Assis",
       img: 'Assets/Images/Quincas.jpeg',
+      preço: 40.00
     },
 ]
 
@@ -21,9 +24,14 @@ Lojinha = () => {
       Produtos.map((val) => {
         containerProdutos.innerHTML+= `
             <div class = produto-single>
-              <img src="`+val.img+`"/>
-              <p>`+val.nome+`</p>
-              <a href="#">Comprar</a>
+              <a href="#">
+                <img src="`+val.img+`"/>
+                <p>`+val.nome+`</p>
+                <p class="autor">`+val.autor+`</p>
+                <div>
+                  <p class="preco"> R$`+val.preço+`</p>
+                </div>
+              </a>
             </div>
         `;
       })
