d5793c48b72ae543407053b701c17d3c01a9523e
Ducaxias <eduardo.caxias90@gmail.com>
2023-11-06 18:58:55 -0300

    feat(Produtos): Adiciona Produtos, esperando API.

--- Assets/js/produtos.js
+++ Assets/js/produtos.js
@@ -1,28 +1,31 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      // Seleciona o ID da seção relacionada ao link
-      const targetId = link.getAttribute('href');
-      // Verifica se a seção já foi carregada
-      if (!document.querySelector(targetId)) {
-        // Cria o elemento div para a seção
-        const section = document.createElement('div');
-        section.id = targetId.slice(1);
-        section.classList.add('section');
-        // Adiciona a div ao final da página
-        document.querySelector('main').appendChild(section);
-        // Mostra o pre-loader enquanto a seção é carregada
-        const preloader = document.createElement('div');
-        preloader.classList.add('preloader');
-        section.appendChild(preloader);
-        // Carrega o conteúdo da seção usando fetch
-        fetch(targetId.slice(1) + '.html')
-          .then(response => response.text())
-          .then(html => {
-            // Remove o pre-loader e adiciona o conteúdo da seção
-            section.removeChild(preloader);
-            section.innerHTML = html;
-          });
-      }
-    });
-  });
+const Produtos = [
+    {
+      nome: "Corto Maltese: balada do mar salgado",
+      autor: "Hugo Pratt",
+      img: 'Assets/Images/Corto.jpeg',
+    },
+    {
+      nome: "Paraíso Perdido",
+      autor: "Jhon Milton",
+      img: 'Assets/Images/Paraiso.jpeg',
+    },
+    {
+      nome: "Quincas Borba",
+      autor: "Machado de Assis",
+      img: 'Assets/Images/Quincas.jpeg',
+    },
+]
+
+Lojinha = () => {
+      var containerProdutos = document.getElementById('produtos');
+      Produtos.map((val) => {
+        containerProdutos.innerHTML+= `
+            <div class = produto-single>
+              <img src="`+val.img+`"/>
+              <p>`+val.nome+`</p>
+              <a href="#">Comprar</a>
+            </div>
+        `;
+      })
+    }
+    Lojinha();
