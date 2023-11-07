f4f36f9b54be11e86563e8544eb77e7c00cc6bba
Arthur Raphael Alves <111453944+ArthurRaphael@users.noreply.github.com>
2023-11-07 15:36:11 -0300

    .

--- Assets/js/produtos.js
+++ Assets/js/produtos.js
@@ -1,21 +1,27 @@
 const Produtos = [
     {
+      id: 0,
       nome: "Corto Maltese <br> Balada do mar salgado",
       autor: "Hugo Pratt",
       img: 'Assets/Images/Corto.jpeg',
-      preço: 25
+      preço: 25,
+      quantidade: 0
     },
     {
+      id: 1,
       nome: "Paraíso Perdido",
       autor: "Jhon Milton",
       img: 'Assets/Images/Paraiso.jpeg',
-      preço: 32
+      preço: 32,
+      quantidade: 0
     },
     {
+      id: 2,
       nome: "Quincas Borba",
       autor: "Machado de Assis",
       img: 'Assets/Images/Quincas.jpeg',
-      preço: 40.00
+      preço: 40.00,
+      quantidade: 0
     },
 ]
 
@@ -23,11 +29,13 @@ Lojinha = () => {
       var containerProdutos = document.getElementById('produtos');
       Produtos.map((val) => {
         containerProdutos.innerHTML+= `
-            <div class = produto-single>
-              <a href="#">
+            <div class = "produto-single c-carrossel-simple">
+              <a class="Clicavel" key=`+val.id+` href="#">
                 <img src="`+val.img+`"/>
-                <p>`+val.nome+`</p>
-                <p class="autor">`+val.autor+`</p>
+                <div class="descricao">
+                  <p>`+val.nome+`</p>
+                  <p class="autor">`+val.autor+`</p>
+                </div>
                 <div>
                   <p class="preco"> R$`+val.preço+`</p>
                 </div>
@@ -37,3 +45,27 @@ Lojinha = () => {
       })
     }
     Lojinha();
+
+Carrinho = () =>{
+  var containerCarrinho = document.getElementById('Carrinho');
+  containerCarrinho.innerHTML = "";
+      Produtos.map((val) => {
+        if(val.quantidade > 0){
+          containerCarrinho.innerHTML+= `
+              <p>Nome:`+val.nome+` | quantidade:`+val.quantidade+`</p> 
+          `;  
+        }
+      })
+}
+    Carrinho();
+
+var links = document.getElementsByClassName("Clicavel")
+
+for(var i= 0; i < links.length; i++){
+  links[i].addEventListener('click', function(){
+    let [key] = this.getAttribute('key');
+    Produtos[key].quantidade++;
+    Carrinho();
+    return false;
+  });
+};
